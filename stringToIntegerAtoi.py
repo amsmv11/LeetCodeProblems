4415878261d24dbd3429a516f3375db071b2ec00
Antonio Vieira <amsmv11@gmail.com>
2020-08-12 16:45:10 +0100

    string to Int Atoi

--- stringToIntegerAtoi.py
+++ stringToIntegerAtoi.py
@@ -1,16 +1,26 @@
-def myAtoi(string):
     res = ""
     sign = 1
-    for i in range(len(string)):
-        if not string[i].isdigit():
-            if string[i] == '-':
-                sign = -1
-            elif string != '+':
-                return 0
-        elif string[i].isdigit():
-            res += string[i]
+    flag = 0
+    for i in range(len(str)):
+        if not str[i].isdigit():
+            if flag == 0:    
+                if str[i] == '-':
+                    sign = -1
+                    flag = 1
+                elif str[i] == '+':
+                    flag = 1
+                elif str[i] != ' ':
+                    return 0
+            else:
+                break
+        elif str[i].isdigit():
+            flag = 1
+            res += str[i]
+    if flag == 0 or res == "":
+        return 0
+    if int(res) >= 2**31:
+        res = 2**31
+        if sign == 1:
+            res -= 1
         
     return int(res)*sign
-
-s = input()
-print(myAtoi(s))        
