5e5dfc9a41ebf6b9bb588b9c9dbcdee3b613d3af
Antonio Vieira <amsmv11@gmail.com>
2020-08-14 16:52:54 +0100

    regular expressions copy from solution

--- regularExpression.py
+++ regularExpression.py
@@ -1,57 +1,12 @@
-def isMatchAux(s, p, lsc, asterisc):
-    if p == ".*":       #aplica se a tudo
-        return True
-    if s == "" and p == "":  #se se acabar a string e o pattern, entao e valido
-        return True
-    if s == "" and len(p) == 2 and p[-1] == "*":   #se se acabar a string, pattern for qq* e valido
-        return True
-    if s != "" and p == "":
-        return False
-    if s == "" and p != "":
-        if asterisc == 1:
-            if len(p) == 1:
-                if p[0] == lsc:
-                    return isMatchAux(s, p[1:], "", 0)
-                if p[0] != lsc:
-                    return False  
-            if p[0] == lsc and 1 < len(p) and p[1] != "*":  #falta verificar se o proximo nao e um asterisco
-                return isMatchAux(s, p[1:], "", 0)
-            if p[1] == "*":
-                return isMatchAux(s, p[2:], lsc, 1) 
-    
+class Solution(object):
+    def isMatch(self, text, pattern):
+        if not pattern:
+            return not text
 
-    else:
-        i = 0
-        j = 0
-        current_char_pattern = p[i]
-        if i+1 < len(p):        #verificar se o char a testar tem * a seguir
-            if p[i+1] == "*":
-                i += 2
-                while j < len(s):
-                    current_char_string = s[j]
-                    if current_char_pattern == current_char_string:
-                        j += 1
-                    else:
-                        break
-                return isMatchAux(s[j:], p[i:], current_char_string, 1)
+        first_match = bool(text) and pattern[0] in {text[0], '.'}
 
-        current_char_string = s[j]   #foi verificado que nao tinha *
-        if current_char_pattern == current_char_string or current_char_pattern == ".":
-            i += 1
-            j += 1
-            return isMatchAux(s[j:], p[i:], current_char_string, 0)
-
-
-
-
-def isMatch(s, p):
-    return isMatchAux(s, p, "", 0)
-    
-    
-
-print(isMatch("aaa", "a*a"))
-    
-    
-                
-
-        
+        if len(pattern) >= 2 and pattern[1] == '*':
+            return (self.isMatch(text, pattern[2:]) or
+                    first_match and self.isMatch(text[1:], pattern))
+        else:
+            return first_match and self.isMatch(text[1:], pattern[1:])
