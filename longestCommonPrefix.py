336348e8cfb408898139133fbb64e9f9f822c247
Antonio Vieira <amsmv11@gmail.com>
2020-08-17 12:39:29 +0100

    longest cmmon prefix

--- longestCommonPrefix.py
+++ longestCommonPrefix.py
@@ -1,29 +1,27 @@
-import difflib
-def longestCommonPrefix(strs):
-    if strs == []:
-        return ""
-    if len(strs) == 1:
-        return strs[0]
-    comparative = strs[0]
-    aux = ""
-    for i in range(1, len(strs)):
-        if aux != "":
-            comparative = aux
-        elif comparative == "":
-            break
-        if strs[i] == "":
-            return ""
-        aux = ""
-        for i,s in enumerate(strs[i]):
-            if i<len(comparative) and comparative[i] == s:  #se os chars forem iguais
-                aux += s
-            else:
-                comparative = aux
-                break
-    if aux != "":
-        return aux
-    else:
-        return comparative
 
-longestCommonPrefix(["abab","aba",""])
-    
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if strs == []:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        comparative = strs[0]
+        aux = ""
+        for i in range(1, len(strs)):
+            if aux != "":
+                comparative = aux
+            elif comparative == "":
+                break
+            if strs[i] == "":
+                return ""
+            aux = ""
+            for i,s in enumerate(strs[i]):
+                if i<len(comparative) and comparative[i] == s:  #se os chars forem iguais
+                    aux += s
+                else:
+                    comparative = aux
+                    break
+        if aux != "":
+            return aux
+        else:
+            return comparative
