2b4c3ff66b60b2887985a6b5194517e2c3ddc2e2
Antonio Vieira <amsmv11@gmail.com>
2020-08-17 14:07:34 +0100

    threesum time limit exceeded

--- threeSum.py
+++ threeSum.py
@@ -1,4 +1,5 @@
 import collections
+import itertools
 
 def threeSum(nums):
     i = 0 
@@ -16,6 +17,9 @@ def threeSum(nums):
         i += 1
         j = i+1
         k = i+2
+    
+    res.sort()
+    resAUX = list(res for res,_ in itertools.groupby(res))
     i = 0
     j = 1
     to_remove = []
@@ -31,7 +35,7 @@ def threeSum(nums):
     to_remove.sort()
     for e in reversed(to_remove):
         del res[e]
-    return res
+    return resAUX
 
-# threeSum([-1,0,1,2,-1,-4])
-threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6])
+threeSum([-1,0,1,2,-1,-4])
+# threeSum([-13,5,13,12,-2,-11,-1,12,-3,0,-3,-7,-7,-5,-3,-15,-2,14,14,13,6,-11,-11,5,-15,-14,5,-5,-2,0,3,-8,-10,-7,11,-5,-10,-5,-7,-6,2,5,3,2,7,7,3,-10,-2,2,-12,-11,-1,14,10,-9,-15,-8,-7,-9,7,3,-2,5,11,-13,-15,8,-3,-7,-12,7,5,-2,-6,-3,-10,4,2,-5,14,-3,-1,-10,-3,-14,-4,-3,-7,-4,3,8,14,9,-2,10,11,-10,-4,-15,-9,-1,-1,3,4,1,8,1])
