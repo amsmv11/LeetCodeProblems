353a5bbbdd5738d219e96ebd6b236c95205a55e9
Antonio Vieira <amsmv11@gmail.com>
2020-08-17 13:57:12 +0100

    threesum time limit exceeded

--- threeSum.py
+++ threeSum.py
@@ -1,3 +1,5 @@
+import collections
+
 def threeSum(nums):
     i = 0 
     j = 1 
@@ -14,6 +16,22 @@ def threeSum(nums):
         i += 1
         j = i+1
         k = i+2
+    i = 0
+    j = 1
+    to_remove = []
+    while i < len(res)-1:
+        while j < len(res):
+            if collections.Counter(res[i]) == collections.Counter(res[j]):
+                # res.remove(res[j])
+                to_remove += [j]
+            j += 1
+        i += 1
+        j = i+1
+    to_remove = list(dict.fromkeys(to_remove))
+    to_remove.sort()
+    for e in reversed(to_remove):
+        del res[e]
     return res
 
-threeSum([-1,0,1,2,-1,-4])
+# threeSum([-1,0,1,2,-1,-4])
+threeSum([-4,-2,-2,-2,0,1,2,2,2,3,3,4,4,6,6])
